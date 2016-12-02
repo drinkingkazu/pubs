9f50c487511608df187f17947e861a99029a0250
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-12-02 15:57:06 -0600

    rotate log script added

---
+++ dstream_online/rotate_logs.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+dir=/home/uboonepro/pubs/log
+
+# Rename log files above a minimum size.
+
+logdir=/datalocal/uboonepro/pubslog/
+
+now=`date +"%d_%m_%Y"`
+
+find $dir -name \*.log | while read log
+do
+  s=`stat -c %s $log`
+  if [ $s -gt 10000 ]; then
+    dest=${logdir}`basename ${log}.${now}`
+    mv -f ${log} ${dest}
+    tar czf ${dest}.tgz ${dest}
+    if [ -e "${dest}.tgz" ] ; then
+	rm -f ${dest}
+	rm -f ${log}
+    else
+	echo "the tarball failed to be created!!!"
+	exit 1
+    fi
+  fi
+done
+
+# Delete unused log files older than 30 days.
+
+find $dir -name \*.log.\* -ctime +30 | while read log
+do
+  rm -f $log
+done
